11fffc109c1f4610c1c9d6d3c7c6d3a6f446c74b
azeez0597 <skazeez8142@gmail.com>
2024-10-20 18:50:57 +0530

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const moviesRoutes = require("./routes/movies");
+require("dotenv").config({ path: "./.env" });
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("Server is running on 3000 port");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/saved", moviesRoutes); 
+
+// const mongoUri = process.env.MONGODB_CONNECTION_LINK;
+
+// const mongoUri =mongodb+srv://praneethvadada25:DMZy38eYUoWShVoU@cluster0.o59ta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// const mongoUri = 'mongodb+srv://praneethvadada25:DMZy38eYUoWShVoU@cluster0.o59ta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoUri = 'mongodb+srv://skazeez:azeez@cluster0.g1okz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const connectToMongo = async () => {
+  try {
+    console.log("trying to connect to mongo wait...");
+    await mongoose.connect(mongoUri);
+    console.log("Connected to mongo Successful");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+connectToMongo();
+
